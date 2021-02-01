11662ee1c20e91b6a761bdc8b82fa29cf3f159e8
Kazion500 <patrickckabwe@gmail.com>
2021-02-01 18:02:16 +0200

    add eye on password field

--- static/js/uploadUserImage.js
+++ static/js/uploadUserImage.js
@@ -54,3 +54,24 @@ imageUpload.addEventListener("change", () => {
     imagesPreview(imageUpload, imgHulgFile);
 });
 }
+
+const togglePassword = document.querySelector('#togglePassword');
+const togglePassword2 = document.querySelector('#togglePassword2');
+const password = document.querySelector('#id_password1');
+const password2 = document.querySelector('#id_password2');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
